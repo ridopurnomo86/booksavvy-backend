16c64be32e2e5e6ca724b4170aaf0eb4854bd98a
ridopurnomo <ridopurnomo86@gmail.com>
2025-07-31 14:13:17 +0700

    fix:remove @data and use @Getter & @Setter for entity

--- src/main/java/com/booksavvy/server/entity/Book.java
+++ src/main/java/com/booksavvy/server/entity/Book.java
@@ -21,7 +21,9 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 enum BookFormat {
     HARDCOVER,
@@ -35,7 +37,8 @@ enum BookFormat {
 @Entity
 @Table(name = "book")
 @EntityListeners(AuditingHandler.class)
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
--- src/main/java/com/booksavvy/server/entity/BookCategory.java
+++ src/main/java/com/booksavvy/server/entity/BookCategory.java
@@ -12,12 +12,14 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "book_category")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
